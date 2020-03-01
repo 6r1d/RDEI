af73415a859ea9943995909107f80803c1875e56
Victor Gridnevsky <vic.6r1d@gmail.com>
2020-03-01 18:58:47 +0300

    Adds comments to mux.h

--- Nimbus/mux.h
+++ Nimbus/mux.h
@@ -1,4 +1,5 @@
-// Mux control pins
+// Mux control pins,
+// outputs to select Mux input channel
 int s0 = 26;
 int s1 = 27;
 int s2 = 28;
@@ -6,35 +7,11 @@ int s3 = 29;
 
 // Mux in "SIG" pin
 int SIG_pin = 30;
-
+// Sets mux control pins
 int controlPin[] = {s0, s1, s2, s3};
 
-void setMuxPin(int channel) {
-  // Encodes mux control bits
-  int muxChannel[16][4] = {
-    {0, 0, 0, 0}, // channel 0
-    {1, 0, 0, 0}, // channel 1
-    {0, 1, 0, 0}, // channel 2
-    {1, 1, 0, 0}, // channel 3
-    {0, 0, 1, 0}, // channel 4
-    {1, 0, 1, 0}, // channel 5
-    {0, 1, 1, 0}, // channel 6
-    {1, 1, 1, 0}, // channel 7
-    {0, 0, 0, 1}, // channel 8
-    {1, 0, 0, 1}, // channel 9
-    {0, 1, 0, 1}, // channel 10
-    {1, 1, 0, 1}, // channel 11
-    {0, 0, 1, 1}, // channel 12
-    {1, 0, 1, 1}, // channel 13
-    {0, 1, 1, 1}, // channel 14
-    {1, 1, 1, 1} // channel 15
-  };
-  // Loop through the 4 sig
-  for (int i = 0; i < 4; i ++) {
-    digitalWrite(controlPin[i], muxChannel[channel][i]);
-  }
-}
-
+// Sets Mux pin modes: one input - SIG_pin,
+// 4 outputs to control multiplexor channel
 void resetMux() {
   pinMode(s0, OUTPUT);
   pinMode(s1, OUTPUT);
@@ -45,4 +22,32 @@ void resetMux() {
   digitalWrite(s1, LOW);
   digitalWrite(s2, LOW);
   digitalWrite(s3, LOW);
+}
+
+// Sets Mux pin channel
+void setMuxPin(int channel) {
+  // Encodes mux control bits,
+  // from 0 to 15
+  int muxChannel[16][4] = {
+    {0, 0, 0, 0}, // 0
+    {1, 0, 0, 0}, // 1
+    {0, 1, 0, 0}, // 2
+    {1, 1, 0, 0}, // 3
+    {0, 0, 1, 0}, // 4
+    {1, 0, 1, 0}, // 5
+    {0, 1, 1, 0}, // 6
+    {1, 1, 1, 0}, // 7
+    {0, 0, 0, 1}, // 8
+    {1, 0, 0, 1}, // 9
+    {0, 1, 0, 1}, // 10
+    {1, 1, 0, 1}, // 11
+    {0, 0, 1, 1}, // 12
+    {1, 0, 1, 1}, // 13
+    {0, 1, 1, 1}, // 14
+    {1, 1, 1, 1}  // 15
+  };
+  // Loop through the 4 sig
+  for (int i = 0; i < 4; i ++) {
+    digitalWrite(controlPin[i], muxChannel[channel][i]);
+  }
 }
